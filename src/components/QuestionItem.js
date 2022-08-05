fbe1621d206c0520ea3fce985d2a7c2adec35491
Bobby Hinton <bob.g.hint@gmail.com>
2022-08-05 17:26:07 -0500

    commit tree 0dc1a7cf91425ac42abaf12b153926a479385cd1

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onClickDelete, onCategoryChange }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,46 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleDelete () {
+    fetch(`http://localhost:4000/questions/${question.id}`,{
+      method: "DELETE",
+    })
+    .then(response => response.json())
+    .then(() => onClickDelete(question))
+  }
+
+  // REMEMBER THAT DELETE DOES NOT RETURN DATA, NO NEED TO .THEN THE RESPONSE DATA  
+
+  // when deleting an item, you only need an anonymous function with no argument passed, 
+  // while passing the deleted question to our onclickdelete... which will set the state
+  // for our questions and filter out the questions with an ID that do not match the deleted
+  // question
+
+  function handleCategoryChange(event) {
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "correctIndex" : event.target.value
+      })
+    })
+    .then(response => response.json())
+    .then(data => onCategoryChange(data))
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select 
+        onChange={handleCategoryChange}
+        defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+
+  const[questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then(response => response.json())
+    .then(data => setQuestions(data))
+  }, [])
+
+  function handleNewQuestion(formData) {
+    setQuestions([...questions, formData])
+  }
+
+  function onClickDelete (deletedQuestion) {
+    console.log(deletedQuestion)
+    const updatedQuestions = questions.filter(value => value.id !== deletedQuestion.id)
+    setQuestions(updatedQuestions)
+  }
+
+  function onCategoryChange(updatedQuestion) {
+    const updatedQuestions = questions.map(value => {
+      if(value.id === updatedQuestion.id){
+        return updatedQuestion
+      } else {
+        return value
+      }
+    })
+    setQuestions(updatedQuestions) // remmeber we must always pass an updated array to our state
+    // setter function in order for it to update state 
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map(value => 
+        <QuestionItem 
+          onCategoryChange={onCategoryChange}
+          handleNewQuestion={handleNewQuestion}
+          onClickDelete={onClickDelete}
+          question={value} 
+          key={value.id} />  
+          )}
+      </ul>
     </section>
   );
 }
